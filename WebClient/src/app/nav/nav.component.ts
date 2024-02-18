1116e02b51a0825f30d54744978e159a19e10e3c
sathish kumar <devsathish05@outlook.com>
2024-02-18 18:38:36 +0530

    Modifiedpersist

--- WebClient/src/app/nav/nav.component.ts
+++ WebClient/src/app/nav/nav.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { AccountService } from '../_services/account.service';
+import { Observable, of } from 'rxjs';
+import { User } from '../-models/users';
 
 @Component({
   selector: 'app-nav',
@@ -8,31 +10,37 @@ import { AccountService } from '../_services/account.service';
 })
 export class NavComponent implements OnInit {
   model:any={};
-  loggedIn=false;
+  //loggedIn=false;   // no flag required instead used below but this also noisy we can 
+  //directly use AccountService directly in our template
+  currentUsers$:Observable<User|null>=of(null)
 
   /**
    *
    */
-  constructor(private accountService: AccountService) {
+  constructor(public  accountService: AccountService) {
  
     
   }
   ngOnInit(): void {
-this.getCurrentUser();
+ //this.getCurrentUser();
+ // directly use AccountService directly in our template
+ // this.currentUsers$=this.accountService.curentUser$;
   }
 
-  getCurrentUser(){
+  // bestway we can use Async pipe so auto subscribe & SUBSCRIBE
+/*   getCurrentUser(){
     this.accountService.curentUser$.subscribe({
       next:user=>this.loggedIn=!!user,//will convert user to bool 
       error:error=>console.log(error)
     })
-  }
+  } */
+
 
   login(){
   this.accountService.login(this.model).subscribe({
     next:response=>{
       console.log(response);
-      this.loggedIn=true;
+     //this.loggedIn=true;
     },
     error : error=>console.log(error)
   })
@@ -40,7 +48,7 @@ this.getCurrentUser();
 
   logout(){
     this.accountService.logout();
-    this.loggedIn=false;
+   //this.loggedIn=false;
   }
 
 }
