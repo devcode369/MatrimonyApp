62a190c72fec11c0ef15c2d59a6a872ded56019a
sathish kumar <devsathish05@outlook.com>
2024-07-21 05:18:28 +0530

    402403replaceNormaltoReactiveform

--- WebClient/src/app/register/register.component.ts
+++ WebClient/src/app/register/register.component.ts
@@ -1,6 +1,7 @@
 import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 import { AccountService } from '../_services/account.service';
 import { ToastrService } from 'ngx-toastr';
+import { FormControl, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
@@ -14,30 +15,42 @@ export class RegisterComponent implements OnInit {
   @Output() cancelRegister =new EventEmitter();  
 
   model:any={}
+
+  registerForm:FormGroup =new FormGroup({});
+
  
   constructor(private accountService:AccountService,private toaster:ToastrService) {
      
     
   }
   ngOnInit(): void {
-  
+  this.initializeForm();
   }
 
-  register(){
-   this.accountService.register(this.model).subscribe({
-  //   next:response=>{
+initializeForm(){
+  this.registerForm=new FormGroup({
+    username:new FormControl(),
+    password:new FormControl(),
+    confirmPassword:new FormControl()
+  })
+}
 
-  // console.log(response);
-  // this.cancel();
-  //  },
-  next:()=>{
+  register(){
+    console.log(this.registerForm?.value);
+  //  this.accountService.register(this.model).subscribe({
+  // //   next:response=>{
+
+  // // console.log(response);
+  // // this.cancel();
+  // //  },
+  // next:()=>{
 
     
-    this.cancel();
-     },
-   //error: error => console.log(error)
-   error: error => this.toaster.error(error.error)
-   })
+  //   this.cancel();
+  //    },
+  //  //error: error => console.log(error)
+  //  error: error => this.toaster.error(error.error)
+  //  })
   }
 
   cancel(){
